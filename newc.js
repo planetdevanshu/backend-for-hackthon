bd098950d8db27064b7d0577fca595da6cd8c0bd
planetdevanshu <devanshu08642@gmail.com>
2024-03-16 05:17:17 +0530

    modules

--- newc.js
+++ newc.js
@@ -6,3 +6,13 @@ if(value === "devanshu"){
     console.log(false)
 }
 console.log("okk so here are the steps to be followed in case i forget the procedure,yes so first of all remember to make a new folder and the start fresh in vs code then clone the github site then make changes in the code and then save it and then save the file IN THE FOLDER OF THE NAME OF THE GITHUB and then its done ")
+const john = 'john'
+const peter = 'peter'
+
+const sayhi = (name) => {
+    console.log('hello there $(name)')
+
+}
+sayhi('susan')
+sayhi(john)
+sayhi(peter)

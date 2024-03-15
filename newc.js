3cbe298ddd7ab1f02a2e7fa88af01a64ee73493f
planetdevanshu <devanshu08642@gmail.com>
2024-03-16 04:59:04 +0530

    a help for the debugging or module problem

--- newc.js
+++ newc.js
@@ -5,3 +5,4 @@ if(value === "devanshu"){
 }else{
     console.log(false)
 }
+console.log("okk so here are the steps to be followed in case i forget the procedure,yes so first of all remember to make a new folder and the start fresh in vs code then clone the github site then make changes in the code and then save it and then save the file IN THE FOLDER OF THE NAME OF THE GITHUB and then its done ")
